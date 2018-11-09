e77221ac67764809d5976d096a643e4563dfe33e
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-09 01:17:25 +0000

    Done.

--- boolean_enumerables.rb
+++ boolean_enumerables.rb
@@ -1,4 +1,4 @@
-#check if music in an artist's playlist is made up of singles, albums, or both
+# check if music in an artist's playlist is made up of singles, albums, or both
 
 playlists = [
   ["Adele",
@@ -15,7 +15,7 @@ playlists = [
 playlists.each do |artist_playlist| 
   artist_playlist.each do |playlist_element|
     if playlist_element.class != Array
-      print "The #{playlist_element} playlist has: " #using 'print' to keep next string on same line
+      print "The #{playlist_element} playlist has: " # print doesn't create new line after string
     elsif playlist_element.all? { |element| element == "single" }
       puts "only singles"
     elsif playlist_element.all? { |element| element == "album" }
