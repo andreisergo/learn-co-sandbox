dc5aa7835cad88d9edce3490bfc51b9c4e4e4f6f
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-13 21:40:06 +0000

    automatically backed up by learn

--- OOP/dog.rb
+++ OOP/dog.rb
@@ -1,6 +1,8 @@
 class Dog 
-  def initialize(dog_name)
+  def initialize(dog_name, dog_breed, dog_size)
     @this_dogs_name = dog_name
+    @this_dogs_breed = dog_breed
+    @this_dogs_size = dog_size
   end
   
   def name=(dog_name)
