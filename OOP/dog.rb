8a97ed96db3daa57610df1e3f41dca46d167f5f8
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-13 21:55:04 +0000

    automatically backed up by learn

--- OOP/dog.rb
+++ OOP/dog.rb
@@ -5,17 +5,30 @@ class Dog
     @this_dogs_size = dog_size
   end
   
-  def param=(dog_params)
+  def name=(dog_name)
     @this_dogs_name = dog_name
   end
   
   def name
     @this_dogs_name
   end
+  
+  def breed=(dog_breed)
+    @this_dogs_breed = dog_breed
+  end
+  
+  def breed
+    @this_dogs_breed
+  end
+  
+  def size=(dog_size)
+    @this_dogs_size = dog_size
+  end
+  
+  def name
+    @this_dogs_size
+  end
 end
 
-dog1 = Dog.new("Lassie")
-dog2 = Dog.new("Snoopy")
-
-puts dog1.name
-puts dog2.name
+dog1 = Dog.new("Lassie", "Maltese", "Medium")
+puts Dog1
